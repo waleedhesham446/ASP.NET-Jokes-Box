7b40d909dfc3f3cb4c4e6d31b495964ff2b453e1
0xwaleedh <waleedhesham2001@gmail.com>
2023-07-30 23:38:57 +0200

    Uploaded Project

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Jokes.Models;
+
+namespace Jokes.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Jokes.Models.Joke> Joke { get; set; } = default!;
+}
